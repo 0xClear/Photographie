57b22ae461c643d01dbb61ea94a0309f0ed3ffc9
Mustafa B <bahadoriborz@gmail.com>
2023-07-19 12:14:32 +0200

    addeed javascript on offers_card

---
+++ js/mouse-position.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function addMousePositionToCss() {
+  const elements = document.querySelectorAll(".mouse-position-css");
+  for (const element of elements) {
+    element.addEventListener("mousemove", function (e) {
+      var rect = element.getBoundingClientRect();
+      var x = e.clientX - rect.left;
+      var y = e.clientY - rect.top;
+
+      element.style.setProperty("--mouse-x", x / element.offsetWidth);
+      element.style.setProperty("--mouse-y", y / element.offsetHeight);
+    });
+
+    element.addEventListener("mouseleave", function (e) {
+      element.style.removeProperty("--mouse-x");
+      element.style.removeProperty("--mouse-y");
+    });
+  }
+}
+if (document.readyState === "complete" || document.readyState === "interactive"){
+    setTimeout(addMousePositionToCss, 1);
+}else{
+    document.addEventListener("DOMContentLoaded", addMousePositionToCss, false);
+}
+
