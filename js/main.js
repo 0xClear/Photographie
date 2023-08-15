c07b2c09e2be616c70a4c75e02515211a5bf0990
Mustafa B <bahadoriborz@gmail.com>
2023-08-15 14:22:51 +0200

    fix cosebutton booking__model-form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-const navbar = document.querySelector('front_id');
-const close = document.querySelector('front_id');
-const open = document.querySelector('front_id');
+"use strict"
 
+function BookingModelfixClose(){
+    const elements = document.querySelectorAll(".booking__model-close");
+    for(const element of elements){
+        element.addEventListener("click", function(){
+            const scrollY = window.scrollY;
+            const scrollX = window.scrollX;
+            setTimeout(function(){
+                window.scrollTo({top: scrollY, left: scrollX});
+            }, 1 );
+        } );
+    }
+}
+
+if (document.readyState === "complete" || document.readyState === "interactive" ){
+    setTimeout(BookingModelfixClose, 1);
+}else{
+    document.addEventListener("DOMContentLoaded", BookingModelfixClose, false);
+}
